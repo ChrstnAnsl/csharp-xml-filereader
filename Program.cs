c7891960e73d28bef79145c6cc75acee13a89d58
Christian <36796016+ChrstnAnsl@users.noreply.github.com>
2023-05-08 13:39:54 +0800

    C# Xml File Reader

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program.GetDataFromXml("homepage", "header");
+    }
+
+    static void GetDataFromXml(String page, String element)
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load("locators.xml");
+        XmlNode pageElement = xmlDoc.SelectSingleNode("//" + page);
+
+        foreach (XmlNode node in pageElement.ChildNodes)
+        {
+            if (node.Name == element)
+            {
+                Console.WriteLine("Page is: " + node.Name + " and the Element is: " + node.InnerText);
+            }
+        }
+    }
+}
